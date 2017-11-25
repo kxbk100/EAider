28b9c6719f9391059e3b77615f2330614ac084d3
冯天祥 <751728440@qq.com>
2017-11-25 17:54:59 +0800

    2017年11月25日17:54:55

---
+++ test/login.php
@@ -0,0 +1,24 @@
+<?php
+	define("LOGIN_SUCCESS", 0);		//µÇÂ¼³É¹¦
+	define("NOT_EXIST", 1);		//ÕËºÅ²»´æÔÚ
+	define("PASSWORD_ERROR", 2);		//ÃÜÂë´íÎó
+	
+	header("Content-Type:text/html;charset=utf-8");
+	$studentId = addslashes($_POST['studentId']);
+	$password = addslashes($_POST['password']);
+	
+	$dsn = "mysql:host=localhost; dbname=stuapp_data";
+	$opt = array("PDO::MYSQL_ATTR_INIT_COMMAND=>'set names utf8'");
+	$pdo = new pdo($dsn,"root","root",$opt);
+	
+	if($result = $pdo->query("select * from stu_info where stu_id='{$studentId}'")->fetch()){
+		if($result['password'] === $password){
+			echo LOGIN_SUCCESS;
+		}else{
+			echo PASSWORD_ERROR;
+		}
+	}else{
+		echo NOT_EXIST;
+	}
+			
+?>
